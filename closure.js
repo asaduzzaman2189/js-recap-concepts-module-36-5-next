5dec0b1549f415dc002be2511d4d3bb6cef49df9
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-10-11 06:51:53 +0600

    36_5-3 Understanding closure and return multiples from function

---
+++ closure.js
@@ -0,0 +1,35 @@
+/* const bank = (owner) => {
+  balance = 0;
+  return (amount) => {
+    // balance=balance+amount;
+    balance += amount;
+    return balance;
+  };
+}; */
+
+const bank = (owner) => {
+  balance = 0;
+  return {
+    deposit: (amount) => {
+      // balance = balance + amount;
+      balance += amount;
+      return balance;
+    },
+    withdraw: (amount) => {
+      // balance = balance - amount;
+      balance -= amount;
+      return balance;
+    },
+  };
+};
+
+const mofijBank = bank("Mofij");
+console.log(mofijBank.deposit(100));
+console.log(mofijBank.deposit(300));
+console.log(mofijBank.deposit(50));
+console.log(mofijBank.balance);
+console.log(mofijBank.deposit(200));
+console.log(mofijBank.withdraw(100));
+console.log(mofijBank.withdraw(200));
+
+// closure হচ্ছে কোনো একটি ফাংশনের ভিতরে যদি কোনো একটি ফাংশনকে কল করা হয় অথবা রিটার্ন করা হয় তবে তার ভিতরের ফাংশনটি তার ইন্টার্নাল প্রোপার্টি access করতে পারে, modify করতে পারে কিন্তু বাহির থেকে এগুলো পরিবর্তন করা যাবে না। কারন সে(যেমন bank ফাংশনটি) একটা closure তৈরি করে।
